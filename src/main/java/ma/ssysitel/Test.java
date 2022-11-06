530732d0b7c2760cba1cac5e184cee11c0b41074
mouhinc <elyoubimouhcine4@gmail.com>
2022-11-06 18:02:00 +0100

    comments

--- src/main/java/ma/ssysitel/Test.java
+++ src/main/java/ma/ssysitel/Test.java
@@ -1,7 +1,11 @@
 package main.java.ma.ssysitel;
 
+/*
+ comment
+ */
 class Test {
     public static void main(String[] args) {
+        // ---------------------------------
         System.out.println("Hello!");
     }
 }
